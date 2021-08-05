189c499ae2269b7d8010680e61f33620853a6e08
Erich <erich.meurer@agapys.com>
2021-08-05 10:31:45 -0300

    first commit

---
+++ VisualStudioStyle/Core/PathControler.cs
@@ -0,0 +1,85 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AgentIDE.Core
+{
+    public class PathControler
+    {
+        public string PathProject { get; set; }
+        private TreeView TreeProject;
+        public enum TypeTree { Folder, Archive, Project }
+        public PathControler(TreeView TreeProject)
+        {
+            this.TreeProject = TreeProject;
+        }
+
+        public void NewProject()
+        {
+            System.Windows.Forms.OpenFileDialog folderBrowser = new System.Windows.Forms.OpenFileDialog();
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            folderBrowser.FileName = "Folder Selection.";
+            if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string folderPath = System.IO.Path.GetDirectoryName(folderBrowser.FileName);
+                PathProject = folderPath;
+                AtualizeProject(TreeProject);
+            }
+        }
+
+        private TreeViewItem GetTreeView(string text, TypeTree type)
+        {
+            TreeViewItem item = new TreeViewItem();
+            item.IsExpanded = false;
+            StackPanel stack = new StackPanel();
+            stack.Orientation = System.Windows.Controls.Orientation.Horizontal;
+            System.Windows.Controls.Label lbl = new System.Windows.Controls.Label();
+            lbl.Content = text;
+            if (type == TypeTree.Folder)
+                stack.Children.Add(new PackIcon { Kind = PackIconKind.Folder });
+            else if (type == TypeTree.Archive)
+                stack.Children.Add(new PackIcon { Kind = PackIconKind.Archive });
+            else if (type == TypeTree.Project)
+                stack.Children.Add(new PackIcon { Kind = PackIconKind.ArchiveAlert });
+            stack.Children.Add(lbl);
+            item.Header = stack;
+            return item;
+        }
+        public void AdditionalTreeProject(string path, System.Windows.Controls.TreeViewItem tree)
+        {
+            if (path.Length > 0 && Directory.Exists(PathProject))
+            {
+                string[] directories = Directory.GetDirectories(path);
+                string[] archives = Directory.GetFiles(path);
+                foreach (var item in directories)
+                {
+                    TreeViewItem treeItem2 = GetTreeView(Path.GetFileName(item), TypeTree.Folder);
+                    tree.Items.Add(treeItem2);
+                    AdditionalTreeProject(item, treeItem2);
+                }
+                foreach (var item in archives)
+                {
+                    tree.Items.Add(GetTreeView(Path.GetFileName(item), TypeTree.Archive));
+
+                }
+            }
+        }
+        public void AtualizeProject(System.Windows.Controls.TreeView tree)
+        {
+            tree.Items.Clear();
+            if (PathProject.Length > 0 && Directory.Exists(PathProject))
+            {
+                TreeViewItem treeItem = GetTreeView(Path.GetFileName(PathProject), TypeTree.Project);
+                AdditionalTreeProject(PathProject, treeItem);
+                tree.Items.Add(treeItem);
+            }
+        }
+    }
+}
---
+++ VisualStudioStyle/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using AgentIDE.Core;
+using AvalonDock.Layout.Serialization;
+using CefSharp.WinForms;
+using MaterialDesignColors;
+using MaterialDesignExtensions.Controls;
+using MaterialDesignExtensions.Themes;
+using MaterialDesignThemes.Wpf;
+using ModernWpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgentIDE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MaterialWindow
+    {
+        public static ChromiumWebBrowser Browser { get; set; }
+        public PathControler _PathControler;
+        System.Windows.Forms.Integration.WindowsFormsHost host =
+                new System.Windows.Forms.Integration.WindowsFormsHost();
+        MaterialDesignThemes.Wpf.PaletteHelper paletteHelper = new MaterialDesignThemes.Wpf.PaletteHelper();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+            dockManager.Theme = new AvalonDock.Themes.Vs2013DarkTheme();
+            ITheme theme = paletteHelper.GetTheme();
+            theme.SetBaseTheme(Theme.Dark);
+            paletteHelper.SetTheme(theme);
+        }
+
+
+        private void MaterialWindow_Initialized(object sender, EventArgs e)
+        {
+            XmlLayoutSerializer serializer = new XmlLayoutSerializer(dockManager);
+            if (File.Exists("Layout.xml"))
+            {
+                using (var stream = new StreamReader("Layout.xml"))
+                {
+                    serializer.Deserialize(stream);
+                }
+            }
+        }
+
+        private void MaterialWindow_Closed(object sender, EventArgs e)
+        {
+            XmlLayoutSerializer serializer = new XmlLayoutSerializer(dockManager);
+            using (var stream = new StreamWriter("Layout.xml"))
+            {
+                serializer.Serialize(stream);
+            }
+        }
+
+
+        private void OpenProject_Click(object sender, RoutedEventArgs e)
+        {
+            _PathControler.NewProject();
+        }
+
+        private void ToggleDarkMode_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)ToggleDarkMode.IsChecked)
+            {
+                ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+                dockManager.Theme = new AvalonDock.Themes.Vs2013DarkTheme();
+                ITheme theme = paletteHelper.GetTheme();
+                theme.SetBaseTheme(Theme.Dark);
+                paletteHelper.SetTheme(theme);
+                var converter = new System.Windows.Media.BrushConverter();
+                var brush = (Brush)converter.ConvertFromString("#2d2d30");
+                GridToolBar.Background = brush;
+            }
+            else
+            {
+                dockManager.Theme = new AvalonDock.Themes.Vs2013LightTheme();
+                ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
+                ITheme theme = paletteHelper.GetTheme();
+                theme.SetBaseTheme(Theme.Light);
+                paletteHelper.SetTheme(theme);
+                var converter = new System.Windows.Media.BrushConverter();
+                var brush = (Brush)converter.ConvertFromString("#eeeef2");
+                GridToolBar.Background = brush;
+            }
+        }
+    }
+}
